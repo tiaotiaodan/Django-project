3940f099b2e7fde15a84cba575d20ae5d45ba431
shichao <shichao@schxck.com>
2020-01-10 15:36:06 +0800

    删除不要的

--- test_project/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\施超学习文件夹\Django-project\test_project\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- 基础实战练习/wsgi_web_server_with_urls.py
+++ 基础实战练习/wsgi_web_server_with_urls.py
@@ -9,11 +9,37 @@ from wsgiref.simple_server import make_server
 # 2.开发好对应 的业务 函数
 # 3.一个请求来了之后，先走路由分发器，如果找到对应的function，就执行，如果没有找到，就返回404错误
 
+
+def book(environ, start_response):
+    print("book page")
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
+    return [bytes('<h1>book page</h1>', encoding='utf-8'), ]
+
+def cloth(environ, start_response):
+    print("cloth page")
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
+    return [bytes('<h1>cloth page</h1>', encoding='utf-8'), ]
+
+#以上定义的函数相当于，html页面
+
+def url_dispacher():
+    urls = {
+        '/book': book,
+        '/cloth': cloth,
+    }
+    return urls
+
 def run_server(environ, start_response):
 
-    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
-    return [bytes('<h1>这美女是哪里的啊!!', encoding='utf-8'), ]
+    url_list = url_dispacher()   #拿到所有的url
+    request_url = environ.get("PATH_INFO")
+    print('request url', request_url)
 
+    if request_url in url_list:
+        func = url_list[request_url](environ, start_response)
+        return  func
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
+    return [bytes('<h1>首页</h1>', encoding='utf-8'), ]
 
 if __name__ == '__main__':
     httpd = make_server('localhost', 8888, run_server)
